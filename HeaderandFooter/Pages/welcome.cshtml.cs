14d59e5c6583f88abe72c2a614f7a394241a9304
Srivani Durga <srivanisiri01@gmail.com>
2023-12-27 14:22:43 +0530

    Add project files.

---
+++ HeaderandFooter/Pages/welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeaderandFooter.Pages.Shared
+{
+    public class welcomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
